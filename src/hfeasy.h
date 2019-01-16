c4e7655c382c975da99643620b091af77be4a051
Luis Alves <ljalvs@gmail.com>
2019-01-16 14:56:26 +0000

    hfeasy: add main header file

---
+++ src/hfeasy.h
@@ -0,0 +1,14 @@
+#ifndef _HFEASY_H_
+#define _HFEASY_H_
+
+#include <hsf.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "config.h"
+#include "gpio.h"
+#include "httpd.h"
+#include "mqttcli.h"
+
+
+#endif
