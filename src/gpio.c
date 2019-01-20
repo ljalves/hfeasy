28cb3c6d42c7e93c83956fca324b29fffae1fd96
Luis Alves <ljalvs@gmail.com>
2019-01-20 23:19:07 +0000

    gpio: move back mappings
    false issue

--- src/gpio.c
+++ src/gpio.c
@@ -1,8 +1,120 @@
+/* HFeasy
+
+Copyright (c) 2019 Luis Alves
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 #include "hfeasy.h"
 
 
-
+#if (defined(__LPT100__)||defined(__LPT100F__)) 
+#ifdef __LPT100F__
+static int module_type = HFM_TYPE_LPT100F;
+#else
+static int module_type = HFM_TYPE_LPT100;
+#endif
+const int hf_gpio_fid_to_pid_map_table[HFM_MAX_FUNC_CODE] =
+{
+	HF_M_PIN(2),	//HFGPIO_F_JTAG_TCK
+	HF_M_PIN(3),	//HFGPIO_F_JTAG_TDO
+	HF_M_PIN(4),	//HFGPIO_F_JTAG_TDI
+	HF_M_PIN(5),	//HFGPIO_F_JTAG_TMS
+	HFM_NOPIN,		//HFGPIO_F_USBDP
+	HFM_NOPIN,		//HFGPIO_F_USBDM
+	HF_M_PIN(39),	//HFGPIO_F_UART0_TX
+	HF_M_PIN(40),	//HFGPIO_F_UART0_RTS
+	HF_M_PIN(41),	//HFGPIO_F_UART0_RX
+	HF_M_PIN(42),	//HFGPIO_F_UART0_CTS
+	
+	HF_M_PIN(27),	//HFGPIO_F_SPI_MISO
+	HF_M_PIN(28),	//HFGPIO_F_SPI_CLK
+	HF_M_PIN(29),	//HFGPIO_F_SPI_CS
+	HF_M_PIN(30),	//HFGPIO_F_SPI_MOSI
+	
+	HFM_NOPIN,	//HFGPIO_F_UART1_TX,
+	HFM_NOPIN,	//HFGPIO_F_UART1_RTS,
+	HFM_NOPIN,	//HFGPIO_F_UART1_RX,
+	HFM_NOPIN,	//HFGPIO_F_UART1_CTS,
+	
+	HFM_NOPIN, //HF_M_PIN(11),	//HFGPIO_F_NLINK
+	HFM_NOPIN, //HF_M_PIN(12),	//HFGPIO_F_NREADY
+	HF_M_PIN(45),	//HFGPIO_F_NRELOAD
+	HFM_NOPIN,	//HFGPIO_F_SLEEP_RQ
+	HFM_NOPIN,	//HFGPIO_F_SLEEP_ON
+	
+	HFM_NOPIN, //HF_M_PIN(18),	//HFGPIO_F_WPS
+	HFM_NOPIN,		//HFGPIO_F_RESERVE1
+	HFM_NOPIN,		//HFGPIO_F_RESERVE2
+	HFM_NOPIN,		//HFGPIO_F_RESERVE3
+	HFM_NOPIN,		//HFGPIO_F_RESERVE4
+	HFM_NOPIN,		//HFGPIO_F_RESERVE5
+	
+	HF_M_PIN(12),	//	buzzer
+	HF_M_PIN(11),	//	relay
+	HF_M_PIN(18),	//	switch/button
+};
+#elif defined(__LPB100__)
+static int module_type = HFM_TYPE_LPB100;
+const int hf_gpio_fid_to_pid_map_table[HFM_MAX_FUNC_CODE]=
+{
+	HF_M_PIN(2),	//HFGPIO_F_JTAG_TCK
+	HFM_NOPIN,	//HFGPIO_F_JTAG_TDO
+	HFM_NOPIN,	//HFGPIO_F_JTAG_TDI
+	HF_M_PIN(5),	//HFGPIO_F_JTAG_TMS
+	HFM_NOPIN,		//HFGPIO_F_USBDP
+	HFM_NOPIN,		//HFGPIO_F_USBDM
+	HF_M_PIN(39),	//HFGPIO_F_UART0_TX
+	HF_M_PIN(40),	//HFGPIO_F_UART0_RTS
+	HF_M_PIN(41),	//HFGPIO_F_UART0_RX
+	HF_M_PIN(42),	//HFGPIO_F_UART0_CTS
+	
+	HF_M_PIN(27),	//HFGPIO_F_SPI_MISO
+	HF_M_PIN(28),	//HFGPIO_F_SPI_CLK
+	HF_M_PIN(29),	//HFGPIO_F_SPI_CS
+	HFM_NOPIN, //HF_M_PIN(30),	//HFGPIO_F_SPI_MOSI
+	
+	HFM_NOPIN,	//HFGPIO_F_UART1_TX,
+	HFM_NOPIN,	//HFGPIO_F_UART1_RTS,
+	HFM_NOPIN,	//HFGPIO_F_UART1_RX,
+	HFM_NOPIN,	//HFGPIO_F_UART1_CTS,
+	
+	HFM_NOPIN, //HF_M_PIN(43),	//HFGPIO_F_NLINK
+	HFM_NOPIN, //HF_M_PIN(44),	//HFGPIO_F_NREADY
+	HF_M_PIN(45),	//HFGPIO_F_NRELOAD
+	HF_M_PIN(7),	//HFGPIO_F_SLEEP_RQ
+	HF_M_PIN(8),	//HFGPIO_F_SLEEP_ON
+		
+	HF_M_PIN(15),		//HFGPIO_F_WPS
+	HFM_NOPIN,		//HFGPIO_F_RESERVE1
+	HFM_NOPIN,		//HFGPIO_F_RESERVE2
+	HFM_NOPIN,		//HFGPIO_F_RESERVE3
+	HFM_NOPIN,		//HFGPIO_F_RESERVE4
+	HFM_NOPIN,		//HFGPIO_F_RESERVE5
+	
+	HF_M_PIN(43),	// led
+	HF_M_PIN(44),	// relay
+	HF_M_PIN(30),	// switch/button
+};
+#else
+#error "Device not supported!"
+#endif
 
 static hftimer_handle_t debounce_timer, recovery_timer;
 static volatile uint8_t key_counter = 0, recovery_counter = 0;
@@ -138,6 +250,13 @@ static void USER_FUNC switch_irqhandler(uint32_t arg1, uint32_t arg2)
 
 void USER_FUNC gpio_init(void)
 {
+	/* init gpio */
+	if (hfgpio_fmap_check(module_type) != 0) {
+		HF_Debug(DEBUG_ERROR,"error: gpio map file error\n");
+		while(1)
+			msleep(1000);
+	}
+
 	hfgpio_configure_fpin(GPIO_RELAY, HFM_IO_OUTPUT_0);
 
 #if defined(__LPT100F__)
