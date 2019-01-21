a7309f610ef66dd1748cac73f5e0fe64e708c8b1
Luis Alves <ljalvs@gmail.com>
2019-01-21 00:35:00 +0000

    buzzer: playing with songs

--- src/buzzer.c
+++ src/buzzer.c
@@ -115,6 +115,65 @@ SOFTWARE.
 #define NOTE_D8  4699
 #define NOTE_DS8 4978
 
+
+int notes[] = { 0,
+262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494,
+523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988,
+1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976,
+2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951
+};
+
+
+const char song1[] = "The Simpsons:d=4,o=5,b=160:c.6,e6,f#6,8a6,g.6,e6,c6,8a,8f#,8f#,8f#,2g,8p,8p,8f#,8f#,8f#,8g,a#.,8c6,8c6,8c6,c6";
+const char song2[] = "Indiana:d=4,o=5,b=250:e,8p,8f,8g,8p,1c6,8p.,d,8p,8e,1f,p.,g,8p,8a,8b,8p,1f6,p,a,8p,8b,2c6,2d6,2e6,e,8p,8f,8g,8p,1c6,p,d6,8p,8e6,1f.6,g,8p,8g,e.6,8p,d6,8p,8g,e.6,8p,d6,8p,8g,f.6,8p,e6,8p,8d6,2c6";
+const char song3[] = "TakeOnMe:d=4,o=4,b=160:8f#5,8f#5,8f#5,8d5,8p,8b,8p,8e5,8p,8e5,8p,8e5,8g#5,8g#5,8a5,8b5,8a5,8a5,8a5,8e5,8p,8d5,8p,8f#5,8p,8f#5,8p,8f#5,8e5,8e5,8f#5,8e5,8f#5,8f#5,8f#5,8d5,8p,8b,8p,8e5,8p,8e5,8p,8e5,8g#5,8g#5,8a5,8b5,8a5,8a5,8a5,8e5,8p,8d5,8p,8f#5,8p,8f#5,8p,8f#5,8e5,8e5";
+const char song4[] = "Entertainer:d=4,o=5,b=140:8d,8d#,8e,c6,8e,c6,8e,2c.6,8c6,8d6,8d#6,8e6,8c6,8d6,e6,8b,d6,2c6,p,8d,8d#,8e,c6,8e,c6,8e,2c.6,8p,8a,8g,8f#,8a,8c6,e6,8d6,8c6,8a,2d6";
+const char song5[] = "Muppets:d=4,o=5,b=250:c6,c6,a,b,8a,b,g,p,c6,c6,a,8b,8a,8p,g.,p,e,e,g,f,8e,f,8c6,8c,8d,e,8e,8e,8p,8e,g,2p,c6,c6,a,b,8a,b,g,p,c6,c6,a,8b,a,g.,p,e,e,g,f,8e,f,8c6,8c,8d,e,8e,d,8d,c";
+const char song6[] = "Xfiles:d=4,o=5,b=125:e,b,a,b,d6,2b.,1p,e,b,a,b,e6,2b.,1p,g6,f#6,e6,d6,e6,2b.,1p,g6,f#6,e6,d6,f#6,2b.,1p,e,b,a,b,d6,2b.,1p,e,b,a,b,e6,2b.,1p,e6,2b.";
+const char song7[] = "Looney:d=4,o=5,b=140:32p,c6,8f6,8e6,8d6,8c6,a.,8c6,8f6,8e6,8d6,8d#6,e.6,8e6,8e6,8c6,8d6,8c6,8e6,8c6,8d6,8a,8c6,8g,8a#,8a,8f";
+const char song8[] = "20thCenFox:d=16,o=5,b=140:b,8p,b,b,2b,p,c6,32p,b,32p,c6,32p,b,32p,c6,32p,b,8p,b,b,b,32p,b,32p,b,32p,b,32p,b,32p,b,32p,b,32p,g#,32p,a,32p,b,8p,b,b,2b,4p,8e,8g#,8b,1c#6,8f#,8a,8c#6,1e6,8a,8c#6,8e6,1e6,8b,8g#,8a,2b";
+const char song9[] = "Bond:d=4,o=5,b=80:32p,16c#6,32d#6,32d#6,16d#6,8d#6,16c#6,16c#6,16c#6,16c#6,32e6,32e6,16e6,8e6,16d#6,16d#6,16d#6,16c#6,32d#6,32d#6,16d#6,8d#6,16c#6,16c#6,16c#6,16c#6,32e6,32e6,16e6,8e6,16d#6,16d6,16c#6,16c#7,c.7,16g#6,16f#6,g#.6";
+const char song10[] = "MASH:d=8,o=5,b=140:4a,4g,f#,g,p,f#,p,g,p,f#,p,2e.,p,f#,e,4f#,e,f#,p,e,p,4d.,p,f#,4e,d,e,p,d,p,e,p,d,p,2c#.,p,d,c#,4d,c#,d,p,e,p,4f#,p,a,p,4b,a,b,p,a,p,b,p,2a.,4p,a,b,a,4b,a,b,p,2a.,a,4f#,a,b,p,d6,p,4e.6,d6,b,p,a,p,2b";
+const char song11[] = "StarWars:d=4,o=5,b=45:32p,32f#,32f#,32f#,8b.,8f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32e6,8c#.6,32f#,32f#,32f#,8b.,8f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32e6,8c#6";
+const char song12[] = "GoodBad:d=4,o=5,b=56:32p,32a#,32d#6,32a#,32d#6,8a#.,16f#.,16g#.,d#,32a#,32d#6,32a#,32d#6,8a#.,16f#.,16g#.,c#6,32a#,32d#6,32a#,32d#6,8a#.,16f#.,32f.,32d#.,c#,32a#,32d#6,32a#,32d#6,8a#.,16g#.,d#";
+const char song13[] = "TopGun:d=4,o=4,b=31:32p,16c#,16g#,16g#,32f#,32f,32f#,32f,16d#,16d#,32c#,32d#,16f,32d#,32f,16f#,32f,32c#,16f,d#,16c#,16g#,16g#,32f#,32f,32f#,32f,16d#,16d#,32c#,32d#,16f,32d#,32f,16f#,32f,32c#,g#";
+const char song14[] = "A-Team:d=8,o=5,b=125:4d#6,a#,2d#6,16p,g#,4a#,4d#.,p,16g,16a#,d#6,a#,f6,2d#6,16p,c#.6,16c6,16a#,g#.,2a#";
+const char song15[] = "Flinstones:d=4,o=5,b=40:32p,16f6,16a#,16a#6,32g6,16f6,16a#.,16f6,32d#6,32d6,32d6,32d#6,32f6,16a#,16c6,d6,16f6,16a#.,16a#6,32g6,16f6,16a#.,32f6,32f6,32d#6,32d6,32d6,32d#6,32f6,16a#,16c6,a#,16a6,16d.6,16a#6,32a6,32a6,32g6,32f#6,32a6,8g6,16g6,16c.6,32a6,32a6,32g6,32g6,32f6,32e6,32g6,8f6,16f6,16a#.,16a#6,32g6,16f6,16a#.,16f6,32d#6,32d6,32d6,32d#6,32f6,16a#,16c.6,32d6,32d#6,32f6,16a#,16c.6,32d6,32d#6,32f6,16a#6,16c7,8a#.6";
+const char song16[] = "Jeopardy:d=4,o=6,b=125:c,f,c,f5,c,f,2c,c,f,c,f,a.,8g,8f,8e,8d,8c#,c,f,c,f5,c,f,2c,f.,8d,c,a#5,a5,g5,f5,p,d#,g#,d#,g#5,d#,g#,2d#,d#,g#,d#,g#,c.7,8a#,8g#,8g,8f,8e,d#,g#,d#,g#5,d#,g#,2d#,g#.,8f,d#,c#,c,p,a#5,p,g#.5,d#,g#";
+const char song17[] = "Gadget:d=16,o=5,b=50:32d#,32f,32f#,32g#,a#,f#,a,f,g#,f#,32d#,32f,32f#,32g#,a#,d#6,4d6,32d#,32f,32f#,32g#,a#,f#,a,f,g#,f#,8d#";
+const char song18[] = "Smurfs:d=32,o=5,b=200:4c#6,16p,4f#6,p,16c#6,p,8d#6,p,8b,p,4g#,16p,4c#6,p,16a#,p,8f#,p,8a#,p,4g#,4p,g#,p,a#,p,b,p,c6,p,4c#6,16p,4f#6,p,16c#6,p,8d#6,p,8b,p,4g#,16p,4c#6,p,16a#,p,8b,p,8f,p,4f#";
+const char song19[] = "MahnaMahna:d=16,o=6,b=125:c#,c.,b5,8a#.5,8f.,4g#,a#,g.,4d#,8p,c#,c.,b5,8a#.5,8f.,g#.,8a#.,4g,8p,c#,c.,b5,8a#.5,8f.,4g#,f,g.,8d#.,f,g.,8d#.,f,8g,8d#.,f,8g,d#,8c,a#5,8d#.,8d#.,4d#,8d#.";
+const char song20[] = "LeisureSuit:d=16,o=6,b=56:f.5,f#.5,g.5,g#5,32a#5,f5,g#.5,a#.5,32f5,g#5,32a#5,g#5,8c#.,a#5,32c#,a5,a#.5,c#.,32a5,a#5,32c#,d#,8e,c#.,f.,f.,f.,f.,f,32e,d#,8d,a#.5,e,32f,e,32f,c#,d#.,c#";
+const char song21[] = "MissionImp:d=16,o=6,b=95:32d,32d#,32d,32d#,32d,32d#,32d,32d#,32d,32d,32d#,32e,32f,32f#,32g,g,8p,g,8p,a#,p,c7,p,g,8p,g,8p,f,p,f#,p,g,8p,g,8p,a#,p,c7,p,g,8p,g,8p,f,p,f#,p,a#,g,2d,32p,a#,g,2c#,32p,a#,g,2c,a#5,8c,2p,32p,a#5,g5,2f#,32p,a#5,g5,2f,32p,a#5,g5,2e,d#,8d";
+
+const char *songs[] = {
+	song1,
+	song2,
+	song3,
+	song4,
+	song5,
+	song6,
+	song7,
+	song8,
+	song9,
+	song10,
+	song11,
+	song12,
+	song13,
+	song14,
+	song15,
+	song16,
+	song17,
+	song18,
+	song19,
+	song20,
+	song21,
+};	
+	
+
+#define isdigit(n) (n >= '0' && n <= '9')
+
+
 static hftimer_handle_t buzzer_timer;
 
 struct buzzer_sound {
@@ -244,12 +303,187 @@ static const struct buzzer_sound *buzzer_tones[] = {
 
 
 static struct buzzer_state_s {
+	const char *song;
 	const struct buzzer_sound *tone;
 	uint16_t freq;
 	uint8_t	cnt;
 } buzzer_state;
-	
 
+#if 1
+
+#define OCTAVE_OFFSET -1
+
+static void USER_FUNC buzzer_timer_handler(hftimer_handle_t timer)
+{
+	static const char *p;
+	static uint8_t state = 0;
+
+  static uint8_t default_dur = 4;
+  static uint8_t default_oct = 6;
+  static int bpm = 63;
+  int num;
+  static long wholenote;
+  long duration;
+  uint8_t note;
+  uint8_t scale;
+	
+u_printf(".%d.", state);
+	switch (state) {
+		case 0:
+			/* init */
+			u_printf("init\r\n");
+			p = buzzer_state.song;
+			
+			if (p == NULL)
+				return;
+			
+			while (*p != ':')
+				p++;
+			p++;
+			
+			// get default duration
+			if(*p == 'd') {
+				p++; p++;              // skip "d="
+				num = 0;
+				while (isdigit(*p)) {
+					num = (num * 10) + (*p++ - '0');
+				}
+				if(num > 0)
+					default_dur = num;
+				p++;                   // skip comma
+			}
+
+			// get default octave
+			if(*p == 'o') {
+				p++; p++;              // skip "o="
+				num = *p++ - '0';
+				if(num >= 3 && num <=7)
+					default_oct = num;
+				p++;                   // skip comma
+			}
+
+			// get BPM
+			if(*p == 'b')
+			{
+				p++; p++;              // skip "b="
+				num = 0;
+				while(isdigit(*p)) {
+					num = (num * 10) + (*p++ - '0');
+				}
+				bpm = num;
+				p++;                   // skip colon
+			}
+
+			// BPM usually expresses the number of quarter notes per minute
+			wholenote = (60 * 1000L / bpm) * 4;  // this is the time for whole note (in milliseconds)
+
+			u_printf("starting tune %d %d\r\n", bpm, wholenote);
+			
+			state = 1;
+			hftimer_start(buzzer_timer);
+			break;
+		case 1:
+			hfgpio_pwm_disable(GPIO_BUZZER);
+			hfgpio_fset_out_low(GPIO_BUZZER);
+			u_printf("p");
+
+			if (*p == '\0') {
+				state = 2;
+				hftimer_change_period(timer, 10); // start(buzzer_timer);
+				break;
+			}
+			
+			// first, get note duration, if available
+			num = 0;
+			while (isdigit(*p))
+				num = (num * 10) + (*p++ - '0');
+    
+			if (num)
+				duration = wholenote / num;
+			else
+				duration = wholenote / default_dur;  // we will need to check if we are a dotted note after
+
+			// now get the note
+			note = 0;
+
+			switch(*p)
+			{
+				case 'c':
+					note = 1;
+					break;
+				case 'd':
+					note = 3;
+					break;
+				case 'e':
+					note = 5;
+					break;
+				case 'f':
+					note = 6;
+					break;
+				case 'g':
+					note = 8;
+					break;
+				case 'a':
+					note = 10;
+					break;
+				case 'b':
+					note = 12;
+					break;
+				case 'p':
+				default:
+					note = 0;
+			}
+			p++;
+
+			// now, get optional '#' sharp
+			if(*p == '#') {
+				note++;
+				p++;
+			}
+
+			// now, get optional '.' dotted note
+			if(*p == '.') {
+				duration += duration/2;
+				p++;
+			}
+  
+			// now, get scale
+			if (isdigit(*p)) {
+				scale = *p - '0';
+				p++;
+			} else {
+				scale = default_oct;
+			}
+
+			scale += OCTAVE_OFFSET;
+//const char song11[] = "StarWars:d=4,o=5,b=45:32p,32f#,32f#,32f#,8b.,8f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32e6,8c#.6,32f#,32f#,32f#,8b.,8f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32e6,8c#6";
+
+			if(*p == ',')
+				p++;       // skip comma for next note (or we may be at the end)
+
+			// now play the note
+			if (note) {
+				uint16_t freq = (uint16_t) notes[(scale - 4) * 12 + note];
+				hfgpio_pwm_enable(GPIO_BUZZER, freq, 1);
+			} else {
+				hfgpio_pwm_disable(GPIO_BUZZER);
+				hfgpio_fset_out_low(GPIO_BUZZER);
+			}
+			u_printf("duraton=%d\r\n", duration);
+			hftimer_change_period(timer, duration); // start(buzzer_timer);
+			break;
+		default:
+		case 2:
+			u_printf("end\r\n");
+			/* end */
+			hfgpio_pwm_disable(GPIO_BUZZER);
+			hfgpio_fset_out_low(GPIO_BUZZER);
+			state = 0;
+			p = NULL;
+			break;
+	}
+}
+#else
 static void USER_FUNC buzzer_timer_handler(hftimer_handle_t timer)
 {
 	switch (buzzer_state.tone->func) {
@@ -290,17 +524,23 @@ static void USER_FUNC buzzer_timer_handler(hftimer_handle_t timer)
 			break;
 	}
 }
+#endif
 
 void USER_FUNC buzzer_play(uint8_t tone)
 {
+#if defined(__LPT100F__)
 	hftimer_stop(buzzer_timer);
 	hfgpio_pwm_disable(GPIO_BUZZER);
 	hfgpio_fset_out_low(GPIO_BUZZER);
 
 	buzzer_state.cnt = 0;
 	buzzer_state.tone = buzzer_tones[tone];
-	hftimer_start(buzzer_timer);
-}
+
+	buzzer_state.song = songs[tone];
+
+	hftimer_change_period(buzzer_timer, 100);
+#endif
+	}
 
 
 
